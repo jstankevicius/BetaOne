32461f4740ecd972f9132941a8cc89aef2f0ca7d
Justas <juuuuuustas@gmail.com>
2018-06-30 18:03:51 -0500

    more training fixes

--- ai/training.py
+++ ai/training.py
@@ -4,7 +4,7 @@ import chess.pgn
 import translator as tr
 import numpy as np
 
-BATCHES = 512                       # how many batches are fed into the neural network
+BATCHES = 15625                     # how many batches are fed into the neural network
 GAMES = 16                          # how many games we sample per batch
 MOVE_SAMPLES = 2                    # how many board positions are sampled per game
 BATCH_SIZE = GAMES * MOVE_SAMPLES   # total number of samples fed into the batch
@@ -19,6 +19,9 @@ RESULT_DICT = {
 # The number of the first game
 START = 17600000
 
+base_board = Board()
+base_tensor = tr.board_tensor(base_board)
+
 agent = Agent()
 agent.build_nn()
 
@@ -27,29 +30,39 @@ for batch in range(BATCHES):
     inputs = np.zeros(shape=(BATCH_SIZE, 8, 8, 12))
     outputs = np.zeros(shape=(BATCH_SIZE, 1))
     index_in_batch = 0
-    print(batch)
 
     for game_number in range(GAMES):
-        pgn = open("D://data//qchess//games//" + str(START + game_number) + ".pgn")
-        game = chess.pgn.read_game(pgn)
-        board = game.board()
+        try:
+            pgn = open("D://data//qchess//games//" + str(START + game_number) + ".pgn")
+            game = chess.pgn.read_game(pgn)
+            board = game.board()
 
-        positions = []
+            positions = []
 
-        for move in game.main_line():
-            positions.append(tr.board_tensor(board))
-            board.push(move)
+            for move in game.main_line():
+                positions.append(tr.board_tensor(board))
+                board.push(move)
 
-        sampled_indices = np.random.randint(low=0, high=len(positions), size=MOVE_SAMPLES, dtype=np.uint32)
+            sampled_indices = np.random.randint(low=0, high=len(positions), size=MOVE_SAMPLES, dtype=np.uint32)
 
-        for i in range(MOVE_SAMPLES):
-            sample_index = sampled_indices[i]
+            for i in range(MOVE_SAMPLES):
+                sample_index = sampled_indices[i]
 
-            inputs[index_in_batch] = positions[sample_index]
-            outputs[index_in_batch] = RESULT_DICT[board.result(claim_draw=True)]
+                inputs[index_in_batch] = positions[sample_index]
+                outputs[index_in_batch] = RESULT_DICT[board.result(claim_draw=True)]
 
-            index_in_batch += 1
+                index_in_batch += 1
 
-            # TODO: handle errors
+        except AttributeError:
+            print("AttributeError at " + str(START + game_number) + ".pgn: cannot read data from board")
 
-    agent.train(inputs, outputs)
+        except UnicodeDecodeError:
+            print("UnicodeDecodeError at " + str(START + game_number) + ".pgn: symbol does not match any recognized")
+
+    loss = agent.train(inputs, outputs)
+    print(str(batch) + ".\tloss: " + str(loss))
+
+    if (batch + 1) % 625 == 0:
+        # perform base evaluation:
+        base_eval = agent.eval(base_tensor).astype(np.str_)
+        agent.get_nn().save("models//model" + str(batch) + "_BE_" + base_eval + ".h5")
